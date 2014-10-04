f81490a4ecb5e7b97c2556cfbc8570b412252bf9
smassey <smassey@laptop-smassey>
2014-10-04 21:38:49 +0200

    minor

--- project_name/urls.py
+++ project_name/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # url(r'^$', '{{ project_name }}.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include(django.contrib.admindocs.urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
